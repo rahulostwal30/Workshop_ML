b33dc347d0de27c3e2e7437ccafc31768dfa0f41
RahulOstwal <rahulostwal479@gmail.com>
2026-09-18 13:23:21 +0530

    logging-exception-setup

---
+++ src/test.py
@@ -0,0 +1,17 @@
+import sys
+from src.exception import CustomException
+
+
+def divide_number() -> float:
+    a = 10
+    b = 5
+    result = a/b
+    return result
+
+if __name__ == "__main__":
+    try:
+        result = divide_number()
+        print(result)
+
+    except Exception as e:
+        raise CustomException(e,sys)
